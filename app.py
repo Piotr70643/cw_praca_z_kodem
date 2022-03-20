f73595dccc94566251cde1fab2d7efb2ecbadb3d
Piotr70643 <Piotr70643@wsb.wroclaw.pl>
2022-03-20 11:32:57 +0100

    pkt6

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Welcome', username='Pior')
+    return render_template('index.html', title='Welcome', username='Piotr')
 
 
 @app.route('/index/<name>')
